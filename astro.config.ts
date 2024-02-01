9055756afaff8dda8bc18185acf21e0c60003252
bholmesdev <hey@bholmes.dev>
2024-02-01 08:03:13 -0500

    refactor: new seed file

--- astro.config.ts
+++ astro.config.ts
@@ -10,23 +10,12 @@ import node from "@astrojs/node";
 
 const Event = defineCollection({
   fields: {
+    id: field.number({ primaryKey: true }),
     name: field.text(),
     description: field.text(),
     ticketPrice: field.number(),
     date: field.date(),
     location: field.text(),
-  },
-  data() {
-    return [
-      {
-        name: "Sampha LIVE in Brooklyn",
-        description:
-          "Sampha is on tour with his new, flawless album Lahai. Come see the live performance outdoors in Prospect Park. Yes, there will be a grand piano 🎹",
-        date: new Date("2024-01-01"),
-        ticketPrice: 10000,
-        location: "Brooklyn, NY",
-      },
-    ];
   },
 });
 const Ticket = defineWritableCollection({
@@ -52,6 +41,16 @@ export default defineConfig({
     collections: {
       Event,
       Ticket,
+    },
+    async data({ seed }) {
+      await seed(Event, {
+        name: "Sampha LIVE in Brooklyn",
+        description:
+          "Sampha is on tour with his new, flawless album Lahai. Come see the live performance outdoors in Prospect Park. Yes, there will be a grand piano 🎹",
+        date: new Date("2024-01-01"),
+        ticketPrice: 10000,
+        location: "Brooklyn, NY",
+      });
     },
   },
 });
