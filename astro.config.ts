3d17dff7032918cce4ede9bfdad30dd2ae0ebddc
bholmesdev <hey@bholmes.dev>
2024-03-11 12:06:39 -0400

    wip: use cloudflare

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import simpleStackForm from "simple-stack-form";
 import db from "@astrojs/db";
-import node from "@astrojs/node";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), simpleStackForm(), db()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
 });
