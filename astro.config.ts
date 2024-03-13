33456c3606e5b41d15310ca248230136ffee6c53
bholmesdev <bholmesdev@gmail.com>
2024-03-13 10:36:46 -0400

    wip: video recording

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import simpleStackForm from "simple-stack-form";
-import db from "@astrojs/db";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), simpleStackForm(), db()],
+  integrations: [preact(), simpleStackForm()],
   output: "server",
   adapter: cloudflare(),
 });
