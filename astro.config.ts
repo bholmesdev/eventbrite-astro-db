299c9ccbac2ca3346e4f90c392e1e2048183a459
bholmesdev <bholmesdev@gmail.com>
2024-03-13 11:19:01 -0400

    Revert "wip: video recording"
    This reverts commit 33456c3606e5b41d15310ca248230136ffee6c53.

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import simpleStackForm from "simple-stack-form";
+import db from "@astrojs/db";
+
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), simpleStackForm()],
+  integrations: [preact(), simpleStackForm(), db()],
   output: "server",
   adapter: cloudflare(),
 });
